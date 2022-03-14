a1a698a1cc1c6ea2744973d26bbd0d4fb79a8a82
Sedat-Games <77297354+Sedat-Games@users.noreply.github.com>
2022-03-14 16:47:19 +0300

    Vibration Logic

--- Assets/Scripts/Managers/VibrationManager.cs
+++ Assets/Scripts/Managers/VibrationManager.cs
@@ -1,32 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using MoreMountains.NiceVibrations;
+﻿using MoreMountains.NiceVibrations;
 using UnityEngine;
 
-public class VibrationManager : MonoBehaviour
+public static class VibrationManager 
 {
-    public static VibrationManager instance;
-
-    void Awake()
-    {
-        if (instance != null && instance != this)
-        {
-            Destroy(this.gameObject);
-        }
-        else
-        {
-            instance = this;
-            DontDestroyOnLoad(instance);
-        }
-    }
-
-
-
-    public void VibrateChoice()
+    public static void VibrateChoice(HapticTypes hapticTypes)
     {
         if (PlayerPrefs.GetInt("GameVibration") == 0)
         {
-            MMVibrationManager.Haptic(HapticTypes.RigidImpact);
+            MMVibrationManager.Haptic(hapticTypes);
         }
     }
 
